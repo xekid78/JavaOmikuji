de286a253bcfe6b93b6041217b5e7091cf4e2326
Makoto Tanabe <xekid78@outlook.com>
2018-01-16 15:34:09 +0900

    if文

---
+++ sample2.java
@@ -0,0 +1,19 @@
+public class sample2 {
+	public static void main(String[] args) {
+		int omikuji = (int)(Math.random() * 6 + 1);
+		if (omikuji == 1) {
+			System.out.println("大吉");
+		} else if (omikuji == 2) {
+			System.out.println("中吉");
+		} else if (omikuji == 3) {
+			System.out.println("小吉");
+		} else if (omikuji == 4) {
+			System.out.println("吉");
+		} else if (omikuji == 5) {
+			System.out.println("凶");
+		} else {
+			System.out.println("大凶");
+		}
+	}
+
+}
